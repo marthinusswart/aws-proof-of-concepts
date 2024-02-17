598cec5a58d78d33f01e9946a54d2333fb06f1db
marthinusswart <matt.swart@icloud.com>
2024-02-18 09:05:43 +1100

    added test class and env settings

---
+++ poc1/src/com/mattswart/TestEnv.java
@@ -0,0 +1,9 @@
+package com.mattswart;
+/**
+ * @author <a href="mailto:matt.swart@icloud.com">Matt Swart</a>
+ */
+public class TestEnv{
+    public static void main(String[] args) {
+        System.out.println("Hello Environment");        
+    }
+ }
